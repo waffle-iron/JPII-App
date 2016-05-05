91abaae6b3e5a1981ed11afa08601eff8e631f54
Andrew Augustine <August712@outlook.com>
2016-05-04 21:37:05 -0400

    Ayeeeee
    Now Hiring: (Only JPII Students)
    - Graphic Artists
    - People who know Swift
    - People who know Xcode
    - Pretty much any student that wants to help.
    
    ~Andrew

---
+++ JPII-App/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  JPII-App
+//
+//  Created by Andrew Augustine on 5/4/16.
+//  Copyright © 2016 Andrew Augustine. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
